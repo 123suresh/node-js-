ce284095d391c189c17fe473c8bafa95c2e3deb8
sureshthapa <suresh.thapa@berrybytes.com>
2022-09-25 09:42:52 +0545

    completed node

--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function() {
-mongoose.connect('mongodb://localhost/vidly')
+mongoose.connect(config.get('db'))
     .then(() => console.log(" connected to vidly mongoDB ... "))
     .catch(err => console.error('Couldnot connect to mongoDB ... ', err))
 }
